0764f64d73dd7b40c7e665fc05ca0554ebdf4bda
vcu-oakesn <oakesn@mymail.vcu.edu>
2021-04-07 19:17:16 -0400

    Let's try that again

--- spiral/homework.py
+++ spiral/homework.py
@@ -1,5 +1,5 @@
 def spiralize(number):
     return_value = 1
-    for i in xrange(3,502,2):
+    for i in xrange(3,5002,2):
         total+=i**2 + (i**2-(i-1)) + (i**2-2*(i-1)) + (i**2-3*(i-1))
     return return_value
