3f6667237ea7195c25a5b9c2afb77a89cb2da3d4
vcu-oakesn <oakesn@mymail.vcu.edu>
2021-04-07 20:03:52 -0400

    Encoire une fois

--- spiral/homework.py
+++ spiral/homework.py
@@ -1,5 +1,7 @@
 def spiralize(number):
     return_value = 1
-    for i in range(3,502,2):
-        total+=i**2 + (i**2-(i-1)) + (i**2-2*(i-1)) + (i**2-3*(i-1))
+    for i in range(3, 502, 2):
+        top_right = i**2
+        for j in range(4):
+            sum += top_right - j * (i - 1)
     return return_value
